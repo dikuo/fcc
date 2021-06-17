06f78beb4d992798ba14d800f9504bf6c5b06a71
dikuo <cai20002000@gmail.com>
2021-06-16 18:37:54 -0700

    Update mean_var_std.py

--- Data Analysis with Python/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
+++ Data Analysis with Python/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -3,16 +3,16 @@ import numpy as np
 def calculate(list):
 
     if (len(list) < 9):
-      raise ValueError("List must contain nine numbers.") 
+      raise ValueError("List must contain nine numbers.")
       return list
     
-    myArray = np.array(list, dtype=float).reshape(3, 3)
+    list = np.array(list).reshape(3, 3)
 
     return {
-      'mean': [np.mean(myArray, axis=0).tolist(), np.mean(myArray, axis=1).tolist(), np.mean(myArray).tolist()],
-      'variance': [np.var(myArray, axis=0).tolist(), np.var(myArray, axis=1).tolist(), np.var(myArray).tolist()],
-      'standard deviation': [np.std(myArray, axis=0).tolist(), np.std(myArray, axis=1).tolist(), np.std(myArray).tolist()],
-      'max': [np.amax(myArray, axis=0).tolist(), np.amax(myArray, axis=1).tolist(), np.amax(myArray).tolist()],
-      'min': [np.amin(myArray, axis=0).tolist(), np.amin(myArray, axis=1).tolist(), np.amin(myArray).tolist()],
-      'sum': [np.sum(myArray, axis=0).tolist(), np.sum(myArray, axis=1).tolist(), np.sum(myArray).tolist()]
+      'mean': [np.mean(list, axis=0).tolist(), np.mean(list, axis=1).tolist(), np.mean(list).tolist()],
+      'variance': [np.var(list, axis=0).tolist(), np.var(list, axis=1).tolist(), np.var(list).tolist()],
+      'standard deviation': [np.std(list, axis=0).tolist(), np.std(list, axis=1).tolist(), np.std(list).tolist()],
+      'max': [np.max(list, axis=0).tolist(), np.max(list, axis=1).tolist(), np.max(list).tolist()],
+      'min': [np.min(list, axis=0).tolist(), np.min(list, axis=1).tolist(), np.min(list).tolist()],
+      'sum': [np.sum(list, axis=0).tolist(), np.sum(list, axis=1).tolist(), np.sum(list).tolist()]
     }
